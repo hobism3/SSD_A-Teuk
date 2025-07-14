d5b9a929f7af8d13f22ae7dde4bcd5f42f42808d
sanghyung.kim <sh03.kim@samsung.com>
2025-07-14 16:35:52 +0900

    feature: add print ssd read return value

---
+++ shell/shell.py
@@ -0,0 +1,50 @@
+class Shell:
+    def __init__(self, ssd=None):
+        self._ssd = ssd
+
+    def run(self):
+        while True:
+            try:
+                cmd = input('Shell> ').strip()
+                if not cmd:
+                    continue
+                parts = cmd.split()
+
+                if parts[0].lower() == 'write':
+                    if len(parts) != 3:
+                        print('[Write] ERROR')
+                        continue
+                    try:
+                        lba = int(parts[1])
+                        data = parts[2]
+                        print(f'[Write] LBA: {lba}, Data: {data}')
+                        self._ssd.write(lba, data)
+                        print('[Write] Done')
+                    except ValueError:
+                        print('[Write] ERROR')
+
+                elif parts[0].lower() == 'read':
+                    if len(parts) != 2:
+                        print('[Read] ERROR')
+                        continue
+                    try:
+                        lba = int(parts[1])
+                        print(f'[Read] LBA: {lba}')
+                        print(self._ssd.read(lba))
+                        print('[Read] Done')
+                    except ValueError:
+                        print('[Read] ERROR')
+
+                elif parts[0].lower() == 'exit':
+                    break
+
+                else:
+                    print('INVALID COMMAND')
+
+            except (EOFError, KeyboardInterrupt):
+                break
+
+
+if __name__ == '__main__':
+    shell = Shell()
+    shell.run()
