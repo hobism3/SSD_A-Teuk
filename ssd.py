96a4f2bbb6364107b9a9ddf43521c5336f149156
SojeongPark <ssjjjjjpppp@gmail.com>
2025-07-15 08:34:27 +0900

    refactor: Use Exception error for Error case
    For error case,
    use exception to handle error process
    
    Signed-off-by: SojeongPark <ssjjjjjpppp@gmail.com>

--- ssd.py
+++ ssd.py
@@ -7,16 +7,19 @@ SSD_OUTPUT_FILE_PATH = os.path.join(OUTPUT_DIR, 'ssd_output.txt')
 SSD_NAND_FILE_PATH = os.path.join(OUTPUT_DIR, 'ssd_nand.txt')
 
 
+class InvalidInputError(Exception):
+    pass
+
+
 class SSD:
     def __init__(self):
         self.initialize_ssd_nand()
         self.initialize_ssd_output()
 
     def initialize_ssd_nand(self) -> None:
-        f = open(SSD_NAND_FILE_PATH, 'w')
-        for i in range(100):
-            f.writelines(f'{i:02d} {INITIAL_VALUE}\n')
-        f.close()
+        with open(SSD_NAND_FILE_PATH, 'w', encoding='utf-8') as f:
+            for i in range(100):
+                f.writelines(f'{i:02d} {INITIAL_VALUE}\n')
 
     def initialize_ssd_output(self) -> None:
         with open(SSD_OUTPUT_FILE_PATH, 'w', encoding='utf-8') as f:
@@ -33,8 +36,6 @@ class SSD:
 
     def validate_value(self, input):
         if input is None:
-            return False
-        if len(input) != 10:
             return False
         if not input.startswith('0x') and not input.startswith('0X'):
             return False
@@ -67,61 +68,60 @@ class SSD:
             f.write('ERROR')
 
     def write(self, line_number, new_content):
-        if not self.validate_address(line_number):
+        try:
+            if not self.validate_address(line_number):
+                raise InvalidInputError()
+            if not self.validate_value(new_content):
+                raise InvalidInputError()
+            line_number = int(line_number)
+
+            with open(SSD_NAND_FILE_PATH, encoding='utf-8') as f:
+                lines = f.readlines()
+
+            lines[line_number] = f'{line_number:02d} {new_content}\n'
+            with open(SSD_NAND_FILE_PATH, 'w', encoding='utf-8') as f:
+                f.writelines(lines)
+
+            self.initialize_ssd_output()
+        except InvalidInputError:
             self.report_error()
-            return
-        if not self.validate_value(new_content):
-            self.report_error()
-            return
-        line_number = int(line_number)
-
-        with open(SSD_NAND_FILE_PATH, encoding='utf-8') as f:
-            lines = f.readlines()
-
-        lines[line_number] = f'{line_number:02d} {new_content}\n'
-        with open(SSD_NAND_FILE_PATH, 'w', encoding='utf-8') as f:
-            f.writelines(lines)
-
-        with open(SSD_OUTPUT_FILE_PATH, 'w', encoding='utf-8') as f:
-            f.write('')
 
 
 def main():
     args = sys.argv[1:]
+    try:
+        if len(args) < 2:
+            print('How to use:')
+            print('  Write: python ssd.py W <adress> <value>')
+            print('  Read: python ssd.py R <address>')
+            raise InvalidInputError()
 
-    if len(args) < 2:
-        print('How to use:')
-        print('  Write: python ssd.py W <adress> <value>')
-        print('  Read: python ssd.py R <address>')
+        mode = args[0].upper()
+
+        if mode == 'W':
+            if len(args) != 3:
+                print('W need 3 arguments: W <address> <value>')
+                raise InvalidInputError()
+            address = args[1]
+            value = args[2]
+
+        elif mode == 'R':
+            if len(args) != 2:
+                print('R need 2 arguments: R <address>')
+                raise InvalidInputError()
+            address = args[1]
+        else:
+            print('Supported modes: W (write), R (read)')
+            raise InvalidInputError()
+
+        ssd = SSD()
+        if mode == 'W':
+            ssd.write(address, value)
+        elif mode == 'R':
+            ssd.read(address)
+    except InvalidInputError:
         SSD.report_error()
         sys.exit(1)
-
-    mode = args[0].upper()
-
-    if mode == 'W':
-        if len(args) != 3:
-            print('W need 3 arguments: W <address> <value>')
-            SSD.report_error()
-            sys.exit(1)
-        address = args[1]
-        value = args[2]
-
-    elif mode == 'R':
-        if len(args) != 2:
-            print('R need 2 arguments: R <address>')
-            SSD.report_error()
-            sys.exit(1)
-        address = args[1]
-    else:
-        print('Supported modes: W (write), R (read)')
-        SSD.report_error()
-        sys.exit(1)
-
-    ssd = SSD()
-    if mode == 'W':
-        ssd.write(address, value)
-    elif mode == 'R':
-        ssd.read(address)
 
 
 if __name__ == '__main__':
