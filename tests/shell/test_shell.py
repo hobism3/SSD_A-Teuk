58a4fbd31369241b3feebc1e95d7c43e5d77495a
saerom <develope.nerd@gmail.com>
2025-07-15 10:18:48 +0900

    refactor: extract common constants

--- tests/shell/test_shell.py
+++ tests/shell/test_shell.py
@@ -1,14 +1,18 @@
 import pytest
-
 from pytest_mock import MockerFixture
 
 from shell import Shell
+from shell_constants import ShellCmd as Cmd
+from shell_constants import ShellMsg as Msg
+from shell_constants import ShellPrefix as Pre
+
+INPUT_FUNCTION = 'builtins.input'
 
 
 def test_shell_write_cmd_has_called(mocker: MockerFixture):
     ssd = mocker.Mock()
     shell = Shell(ssd)
-    mocker.patch('builtins.input', side_effect=['write 3 0xEEEEFFFF', 'exit'])
+    mocker.patch(INPUT_FUNCTION, side_effect=[f'{Cmd.WRITE} 3 0xEEEEFFFF', Cmd.EXIT])
 
     shell.run()
 
@@ -17,7 +21,7 @@ def test_shell_write_cmd_has_called(mocker: MockerFixture):
 
 def test_shell_write_cmd(capsys: pytest.CaptureFixture, mocker: MockerFixture):
     ssd = mocker.Mock()
-    mocker.patch('builtins.input', side_effect=['write 32 0xEEEEFFFF', 'exit'])
+    mocker.patch(INPUT_FUNCTION, side_effect=[f'{Cmd.WRITE} 32 0xEEEEFFFF', Cmd.EXIT])
     shell = Shell(ssd)
 
     shell.run()
@@ -25,7 +29,8 @@ def test_shell_write_cmd(capsys: pytest.CaptureFixture, mocker: MockerFixture):
     captured = capsys.readouterr()
     output = captured.out
 
-    assert '[Write] Done' in output
+    assert Msg.DONE in output
+    assert Pre.WRITE in output
 
 
 def test_shell_write_cmd_invalid_param_count(
@@ -33,30 +38,12 @@ def test_shell_write_cmd_invalid_param_count(
 ):
     ssd = mocker.Mock()
     mocker.patch(
-        'builtins.input',
-        side_effect=['write', 'write 98', 'write 99 0xEEEEFFFF 16', 'exit'],
-    )
-    shell = Shell(ssd)
-
-    shell.run()
-
-    captured = capsys.readouterr()
-    output = captured.out
-
-    assert '[Write] ERROR' in output
-
-
-def test_shell_write_cmd_invalid_param_format(
-    capsys: pytest.CaptureFixture, mocker: MockerFixture
-):
-    ssd = mocker.Mock()
-    mocker.patch(
-        'builtins.input',
+        INPUT_FUNCTION,
         side_effect=[
-            'write 100 0xEEEEFFFF',
-            'write 0 0xEEEEFFFFE',
-            'write 99 EEEEFFFF',
-            'exit',
+            Cmd.WRITE,
+            f'{Cmd.WRITE} 98',
+            f'{Cmd.WRITE} 99 0xEEEEFFFF 16',
+            Cmd.EXIT,
         ],
     )
     shell = Shell(ssd)
@@ -66,13 +53,37 @@ def test_shell_write_cmd_invalid_param_format(
     captured = capsys.readouterr()
     output = captured.out
 
-    assert '[Write] ERROR' in output
+    assert Msg.ERROR in output
+    assert Pre.WRITE in output
+
+
+def test_shell_write_cmd_invalid_param_format(
+    capsys: pytest.CaptureFixture, mocker: MockerFixture
+):
+    ssd = mocker.Mock()
+    mocker.patch(
+        INPUT_FUNCTION,
+        side_effect=[
+            f'{Cmd.WRITE} 100 0xEEEEFFFF',
+            f'{Cmd.WRITE} 0 0xEEEEFFFFE',
+            f'{Cmd.WRITE} 99 EEEEFFFF',
+            Cmd.EXIT,
+        ],
+    )
+    shell = Shell(ssd)
+
+    shell.run()
+
+    captured = capsys.readouterr()
+    output = captured.out
+    assert Msg.ERROR in output
+    assert Pre.WRITE in output
 
 
 def test_shell_invalid_cmd(capsys: pytest.CaptureFixture, mocker: MockerFixture):
     ssd = mocker.Mock()
     mocker.patch(
-        'builtins.input', side_effect=['writ 3 0xEEEEFFFF', 'rea 3', 'exi', 'exit']
+        INPUT_FUNCTION, side_effect=['writ 3 0xEEEEFFFF', 'rea 3', 'exi', Cmd.EXIT]
     )
     shell = Shell(ssd)
 
@@ -81,13 +92,13 @@ def test_shell_invalid_cmd(capsys: pytest.CaptureFixture, mocker: MockerFixture)
     captured = capsys.readouterr()
     output = captured.out
 
-    assert 'INVALID COMMAND' in output
+    assert Msg.INVALID in output
 
 
 @pytest.mark.timeout(1)
 def test_shell_exit_cmd_breaks_loop(mocker: MockerFixture):
     ssd = mocker.Mock()
     shell = Shell(ssd)
-    mocker.patch('builtins.input', side_effect=['exit'])
+    mocker.patch(INPUT_FUNCTION, side_effect=[Cmd.EXIT])
 
     shell.run()
