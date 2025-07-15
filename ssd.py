2780f04b955d3d5295ac7289db0958229442980f
astralmin <oz101@naver.com>
2025-07-15 10:19:32 +0900

    refactor: remove duplicate assign data code

--- ssd.py
+++ ssd.py
@@ -51,10 +51,9 @@ class SSD:
             for line in f:
                 data = line.strip().split(' ')
                 ind = int(data[0])
-                value = data[1]
 
                 if int(line_number) == ind:
-                    ret_value = value
+                    ret_value = data[1]
 
         with open(SSD_OUTPUT_FILE_PATH, 'w') as f:
             f.write(f'{ret_value}')
