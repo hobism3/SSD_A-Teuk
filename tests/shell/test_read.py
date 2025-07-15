ba0dc8eadfddffaf3f880bbda1b31e23862ae214
sanghyung.kim <sh03.kim@samsung.com>
2025-07-15 19:58:11 +0900

    test: add read test

---
+++ tests/shell/test_read.py
@@ -0,0 +1,47 @@
+import pytest
+from pytest_mock import MockerFixture
+
+from shell import Shell
+
+
+def test_shell_read(capsys: pytest.CaptureFixture, mocker: MockerFixture):
+    ssd = mocker.Mock()
+
+    mock_process = mocker.Mock()
+    mock_process.returncode = 0
+
+    mock_run = mocker.patch('subprocess.run', return_value=mock_process)
+    mocker.patch('builtins.input', side_effect=['read 00', 'exit'])
+    mocker.patch('builtins.open', mocker.mock_open(read_data='0x00000000'))
+
+    shell = Shell(ssd)
+    shell.run()
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    assert '[Read]  LBA 00: 0x00000000' in output
+    mock_run.assert_called_with(
+        [
+            'python',
+            'C:\\Users\\User\\PycharmProjects\\pythonProject31\\ssd.py',
+            'R',
+            '00',
+        ],
+        check=True,
+    )
+
+
+def test_shell_read_exception(capsys: pytest.CaptureFixture, mocker: MockerFixture):
+    ssd = mocker.Mock()
+    ssd.read.side_effect = [ValueError]
+
+    mocker.patch('builtins.input', side_effect=['read 0 0 0', 'exit'])
+
+    shell = Shell(ssd)
+    shell.run()
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    assert '[Read]  ERROR' in output
