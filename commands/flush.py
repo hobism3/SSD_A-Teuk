ac3bf267c8715320081f58782d11c3780e8f09f3
codepyo <robin5544@naver.com>
2025-07-16 16:03:18 +0900

    feat: add flush command

---
+++ commands/flush.py
@@ -0,0 +1,33 @@
+import subprocess
+from subprocess import CalledProcessError
+
+from commands.base import Command
+from logger import Logger
+from shell_constants import RUN_SSD
+from shell_constants import ShellMsg as Msg
+from shell_constants import ShellPrefix as Pre
+
+
+class FlushCommand(Command):
+    def __init__(self):
+        self._logger = Logger(Pre.FLUSH)
+
+    def parse(self, args: list[str]) -> list[str]:
+        if args:
+            raise ValueError(Msg.FLUSH_HELP)
+        return ['E']
+
+    def parse_result(self, result) -> str:
+        if result == 0:
+            return Msg.DONE
+        return Msg.ERROR
+
+    def execute(self, args: list[str]) -> bool:
+        try:
+            ssd_args = self.parse(args)
+            return_code = subprocess.run(RUN_SSD + ssd_args, check=True)
+            result = self.parse_result(return_code.returncode)
+            self._logger.info(result)
+            return True
+        except (ValueError, CalledProcessError):
+            self._logger.error(Msg.ERROR)
---
+++ tests/shell/test_flush.py
@@ -0,0 +1,61 @@
+from subprocess import CalledProcessError
+
+import pytest
+from pytest_mock import MockerFixture
+
+from shell import Shell
+from shell_constants import RUN_SSD
+from shell_constants import ShellMsg as Msg
+from shell_constants import ShellPrefix as Pre
+
+
+def test_shell_read(capsys: pytest.CaptureFixture, mocker: MockerFixture):
+    mock_process = mocker.Mock()
+    mock_process.returncode = 0
+
+    mock_run = mocker.patch('subprocess.run', return_value=mock_process)
+    mocker.patch('builtins.input', side_effect=['flush', 'exit'])
+
+    shell = Shell()
+    shell.run()
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    assert Pre.FLUSH + ' ' + Msg.DONE in output
+    mock_run.assert_called_with(
+        RUN_SSD + ['E'],
+        check=True,
+    )
+
+
+def test_shell_read_exception_called_process_error(
+    capsys: pytest.CaptureFixture, mocker: MockerFixture
+):
+    mocker.patch('builtins.input', side_effect=['flush', 'exit'])
+    mocker.patch(
+        'subprocess.run',
+        side_effect=CalledProcessError(returncode=1, cmd='dummy command'),
+    )
+
+    shell = Shell()
+    shell.run()
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    assert Pre.FLUSH + ' ' + Msg.ERROR in output
+
+
+def test_shell_read_exception_value_error(
+    capsys: pytest.CaptureFixture, mocker: MockerFixture
+):
+    mocker.patch('builtins.input', side_effect=['flush 0 0', 'exit'])
+
+    shell = Shell()
+    shell.run()
+
+    captured = capsys.readouterr()
+    output = captured.out
+
+    assert Pre.FLUSH + ' ' + Msg.ERROR in output
