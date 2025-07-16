1b2cbeb78bb358e515f414ca670e51333b6771b3
junt-lee <dlwnsxo98@naver.com>
2025-07-16 12:23:59 +0900

    test: add shell-script1,3 tc

--- tests/shell/script/test_script3.py
+++ tests/shell/script/test_script3.py
@@ -1,9 +1,41 @@
-from unittest.mock import mock_open
+from pathlib import Path
+from unittest.mock import call, mock_open
+
+import pytest
+from pytest_mock import MockerFixture
 
 from shell import Shell
+from shell_constants import ShellMsg as Msg
 
 
-def test_script3_success_with_fullname(mocker):
+@pytest.fixture
+def case_list():
+    ssd_path = Path(__file__).resolve().parents[3] / 'ssd.py'
+
+    test_args_list = [
+        ('W', '0', '0xAAAABBBB'),
+        ('W', '99', '0xAAAABBBB'),
+        ('R', '0', None),
+        ('R', '99', None),
+    ]
+
+    test_cases = []
+    for cmd, code, value in test_args_list:
+        cmd_args = [
+            'python',
+            str(ssd_path),
+            cmd,
+            code,
+        ]
+        if value is not None:
+            cmd_args.append(value)
+
+        test_cases.append(call(cmd_args, check=True))
+
+    return test_cases
+
+
+def test_script3_call_subprocess_and_file_with_fullname(mocker):
     mock_run = mocker.patch('commands.base.subprocess.run')
     mock_open_file = mocker.patch('builtins.open', mock_open(read_data=''))
     mocker.patch('builtins.input', side_effect=['3_WriteReadAging', 'exit'])
@@ -12,13 +44,11 @@ def test_script3_success_with_fullname(mocker):
     shell = Shell()
     shell.run()
 
-    # subprocess.run()이 정확한 파라미터로 호출되었는지 검증
-    print(mock_run.call_args)
     assert mock_run.called
     assert mock_open_file.called
 
 
-def test_script3_success_with_shortname(mocker):
+def test_script3_call_subprocess_and_file_with_shortname(mocker):
     mock_run = mocker.patch('commands.base.subprocess.run')
     mock_open_file = mocker.patch('builtins.open', mock_open(read_data=''))
     mocker.patch('builtins.input', side_effect=['3_', 'exit'])
@@ -27,7 +57,46 @@ def test_script3_success_with_shortname(mocker):
     shell = Shell()
     shell.run()
 
-    # subprocess.run()이 정확한 파라미터로 호출되었는지 검증
-    print(mock_run.call_args)
     assert mock_run.called
     assert mock_open_file.called
+
+
+def test_script3_pass_with_no_include_any_error(
+    capsys: pytest.CaptureFixture, mocker: MockerFixture, case_list
+):
+    mock_process = mocker.Mock()
+    mock_process.returncode = 0
+    mock_run = mocker.patch('subprocess.run', return_value=mock_process)
+    mocker.patch('builtins.input', side_effect=['3_', 'exit'])
+    mocker.patch('random.getrandbits', return_value=0xAAAABBBB)
+    mocker.patch('builtins.open', mocker.mock_open(read_data='0xAAAABBBB'))
+    mocker.patch('commands.write.WriteCommand.parse_result', return_value=Msg.DONE)
+
+    shell = Shell()
+    shell.run()
+
+    captured = capsys.readouterr()
+    output = captured.out
+    assert '[3_WriteReadAging] PASS' in output
+    assert 'ERROR' not in output
+
+    for case in case_list:
+        assert case in mock_run.call_args_list
+
+
+def test_script3_valid_call_args_list(
+    capsys: pytest.CaptureFixture, mocker: MockerFixture, case_list
+):
+    mock_process = mocker.Mock()
+    mock_process.returncode = 0
+    mock_run = mocker.patch('subprocess.run', return_value=mock_process)
+    mocker.patch('builtins.input', side_effect=['3_', 'exit'])
+    mocker.patch('random.getrandbits', return_value=0xAAAABBBB)
+    mocker.patch('builtins.open', mocker.mock_open(read_data='0xAAAABBBB'))
+    mocker.patch('commands.write.WriteCommand.parse_result', return_value=Msg.DONE)
+
+    shell = Shell()
+    shell.run()
+
+    for case in case_list:
+        assert case in mock_run.call_args_list
