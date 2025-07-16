d8c9c5f4961a9dfc1c7bcde2067ea448a995b809
astralmin <oz101@naver.com>
2025-07-16 13:47:30 +0900

    feat: make ./buffer folder & #_empty files

--- buffer.py
+++ buffer.py
@@ -1,3 +1,9 @@
+import os
+from pathlib import Path
+
+BUFFER_DIR_NAME = 'buffer'
+BUFFER_DIR = f'{os.path.dirname(os.path.abspath(__file__))}/{BUFFER_DIR_NAME}'
+
 class Buffer:
     _instance = None
 
@@ -7,7 +13,20 @@ class Buffer:
         return cls._instance
 
     def __init__(self):
-        ...
+        self._create_directory(BUFFER_DIR)
+        if os.listdir(BUFFER_DIR) == []:
+            self._create_file(BUFFER_DIR)
+
+    def _create_file(self, dir):
+        for i in range(1, 6):
+            Path(f"{dir}/{i}_empty").touch()
+
+    def _create_directory(self, dir):
+        try:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+        except OSError:
+            print("Creation of the directory %s failed" % dir)
 
     def buffer_file_read(self) -> list:
         ...
