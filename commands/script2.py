20ff86b788061d140215906f725216ed58e60214
Saerom Choi <develope.nerd@gmail.com>
2025-07-18 04:36:01 +0900

    refactor: apply adaptor pattern

--- commands/script2.py
+++ commands/script2.py
@@ -1,24 +1,25 @@
-import random
-
-from commands.script import ScriptCommand
+from commands.base import Command
+from commands.mixin import WriteSupportMixin, RandomValueGenerateMixin, ReadSupportMixin
 from shell_tool.shell_constants import ShellMsg
-from shell_tool.shell_constants import ShellPrefix as Pre
+from shell_tool.shell_constants import ShellPrefix
 from shell_tool.shell_logger import Logger
 
 
-class PartialLBAWriteCommand(ScriptCommand):
-    def __init__(self, logger: Logger, prefix=Pre.SCRIPT_2):
+class PartialLBAWriteCommand(WriteSupportMixin, RandomValueGenerateMixin, ReadSupportMixin,
+    Command):
+    expected_num_args = 0
+    help_msg = ShellMsg.SCRIPT_2_HELP
+
+    def __init__(self, logger: Logger, prefix=ShellPrefix.SCRIPT_2):
         super().__init__(logger, prefix)
-        self._lba = None
-        self._random_value = random.randint(0x00000000, 0xFFFFFFFF)
 
     def execute(self, args=None) -> bool:
+        sample_index = ['4', '0', '3', '1', '2']
         try:
-            sample_index = ['4', '0', '3', '1', '2']
-            self._logger.print_blank_line()
+            self._parse(args)
             self._logger.print_and_log(self._prefix)
             for _ in range(30):
-                hex_string = f'0x{self._random_value:08X}'
+                hex_string = self.rand32()
                 for index in sample_index:
                     self.write(index, hex_string)
 
@@ -27,7 +28,7 @@ class PartialLBAWriteCommand(ScriptCommand):
                     if not success:
                         self._logger.print_and_log(self._prefix, ShellMsg.FAIL)
                         return True
-                self._logger.print_and_log(self._prefix, ShellMsg.PASS)
+            self._logger.log(ShellMsg.PASS)
         except ValueError:
             self._logger.print_and_log(self._prefix, ShellMsg.ERROR)
         return True
